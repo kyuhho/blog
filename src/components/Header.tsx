59a2bd1f01089fe5c83d52ccc5b8a2adf63c4990
kyuhho <rbgh0114@naver.com>
2024-03-30 01:51:33 +0900

    style: init eslint, prettier

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-const Header :React.FC =()=>{
-    return <header className="flex bg-black w-full h-5"></header>;
-}
+const Header: React.FC = () => {
+  return <header className="flex bg-black w-full h-5"></header>;
+};
 
 export default Header;
