3561ee67d491aa6ea2d276c73649ddb2a4697c32
kyuhho <rbgh0114@naver.com>
2024-03-30 22:50:33 +0900

    chore: fix logo color

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link';
+
 const Logo: React.FC = () => {
   return (
-    <div className="flex">
-      <div className="text-4xl text-teal-600 font-bold">{'<'}</div>
-      <div className="text-4xl text-gray-300 font-bold">{'Dev'}</div>
-      <div className="text-4xl text-teal-600 font-bold">{'/>'}</div>
-    </div>
+    <Link href="/">
+      <div className="flex">
+        <div className="text-4xl text-teal-400 font-bold">{'<'}</div>
+        <div className="text-4xl text-gray-300 font-bold">{'Dev'}</div>
+        <div className="text-4xl text-teal-400 font-bold">{'/>'}</div>
+      </div>
+    </Link>
   );
 };
 
