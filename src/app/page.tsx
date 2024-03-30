1a3c3a23de827df5d7f61d8adbae1b8e29e02c9e
kyuhho <rbgh0114@naver.com>
2024-03-30 19:33:24 +0900

    feat: init Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div color="">안녕하세요, 이규호입니다.</div>
+    </main>
   );
 }
