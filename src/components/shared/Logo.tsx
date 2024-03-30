27f3e864217a72b7f6ebaf57104f78d74a84f359
kyuhho <rbgh0114@naver.com>
2024-03-30 19:33:03 +0900

    feat: change logo word

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -2,8 +2,8 @@ const Logo: React.FC = () => {
   return (
     <div className="flex">
       <div className="text-4xl text-cyan-600 font-bold">{'<'}</div>
-      <div className="text-4xl text-gray-300 font-bold">{'295'}</div>
-      <div className="text-4xl text-cyan-600 font-bold">{'>'}</div>
+      <div className="text-4xl text-gray-300 font-bold">{'Dev'}</div>
+      <div className="text-4xl text-cyan-600 font-bold">{'/>'}</div>
     </div>
   );
 };
