086be8944a1a9a667e725d2aca07d55f3f418fe0
kyuhho <rbgh0114@naver.com>
2024-03-30 20:50:57 +0900

    feat: change color cyan to teal

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,9 +1,9 @@
 const Logo: React.FC = () => {
   return (
     <div className="flex">
-      <div className="text-4xl text-cyan-600 font-bold">{'<'}</div>
+      <div className="text-4xl text-teal-600 font-bold">{'<'}</div>
       <div className="text-4xl text-gray-300 font-bold">{'Dev'}</div>
-      <div className="text-4xl text-cyan-600 font-bold">{'/>'}</div>
+      <div className="text-4xl text-teal-600 font-bold">{'/>'}</div>
     </div>
   );
 };
