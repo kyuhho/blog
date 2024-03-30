86c24e2fca1717482f6342ed05e95cc1db05b4d2
kyuhho <rbgh0114@naver.com>
2024-03-30 20:51:59 +0900

    feat: add tech section

---
+++ src/components/home/TechSection.tsx
@@ -0,0 +1,10 @@
+const TechSection: React.FC = () => {
+  return (
+    <section>
+      <h1 className="text-teal-50">TECH elcie font</h1>
+      <h1 className="text-teal-50 text-5xl">테스트 elice text</h1>
+    </section>
+  );
+};
+
+export default TechSection;
