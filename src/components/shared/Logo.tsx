bac5ad69eee23cc458b0f2178370064240e76527
kyuhho <rbgh0114@naver.com>
2024-03-30 17:39:15 +0900

    feat: add logo component

---
+++ src/components/shared/Logo.tsx
@@ -0,0 +1,11 @@
+const Logo: React.FC = () => {
+  return (
+    <div className="flex">
+      <div className="text-4xl text-cyan-600 font-bold">{'<'}</div>
+      <div className="text-4xl text-gray-300 font-bold">{'295'}</div>
+      <div className="text-4xl text-cyan-600 font-bold">{'>'}</div>
+    </div>
+  );
+};
+
+export default Logo;
