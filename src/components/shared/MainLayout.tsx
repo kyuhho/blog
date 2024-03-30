96e7b9367e0eae6fab2e718b986a57243bca2bd5
kyuhho <rbgh0114@naver.com>
2024-03-30 21:08:45 +0900

    feat: add justify center

--- src/components/shared/MainLayout.tsx
+++ src/components/shared/MainLayout.tsx
@@ -6,7 +6,11 @@ interface MainLayoutProps {
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
-    <main className="flex min-h-screen flex-col items-center">{children}</main>
+    <main className="flex min-h-screen justify-center">
+      <div className=" flex flex-col items-center" style={{ width: '75rem' }}>
+        {children}
+      </div>
+    </main>
   );
 };
 
