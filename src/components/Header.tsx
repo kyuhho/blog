86ffc64214cb7a162beeba054cc2575b104b3eea
kyuhho <rbgh0114@naver.com>
2024-03-30 16:26:30 +0900

    feat: add github icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,20 @@
+import Image from 'next/image';
+
 const Header: React.FC = () => {
-  return <header className="flex bg-black w-full h-5"></header>;
+  return (
+    <header className="flex bor w-full border-b-2 border-l-sky-600 h-20">
+      <nav>
+        <div className="flex relative">
+          <Image
+            src={'/icons/github.svg'}
+            width={20}
+            height={20}
+            alt="githubImage"
+          />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
