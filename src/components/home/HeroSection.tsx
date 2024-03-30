e7d23def126546c2bf9f0d64d4e50d022a9d05bc
kyuhho <rbgh0114@naver.com>
2024-03-30 20:51:22 +0900

    feat: use section tag

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,5 +1,13 @@
 const HeroSection: React.FC = () => {
-  return <></>;
+  return (
+    <section>
+      <div className="text-teal-50">
+        저는 사용자에게 더 나은 경험을 제공하고 효율적인 개발을 하기 위해
+        노력하는 프론트엔드 개발자 입니다. 매 순간 도전의 과정에서 고민과
+        합리적인 사고를 통해 성장하고, 새로운 기술을 공부하는 것을 좋아합니다.
+      </div>
+    </section>
+  );
 };
 
 export default HeroSection;
