089acb915727cd7fa1d7a8a12a4499cd21218a07
kyuhho <rbgh0114@naver.com>
2024-03-30 17:36:25 +0900

    build: apply svgr webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //자동으로 웹팩이 build 시에 svg 파일을 react component화 시켜준다.
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
