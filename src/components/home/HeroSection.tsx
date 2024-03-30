d5b8d3fe3c6da25bbe63db0cb5f587f76da9ae30
kyuhho <rbgh0114@naver.com>
2024-03-30 22:50:02 +0900

    feat: apply lottie

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,10 +1,36 @@
+'use client';
+import Lottie from 'react-lottie-player';
+import backgroundLottie from 'public/lotties/backgroundLottie.json';
+
 const HeroSection: React.FC = () => {
   return (
-    <section>
-      <div className="text-teal-50">
-        저는 사용자에게 더 나은 경험을 제공하고 효율적인 개발을 하기 위해
-        노력하는 프론트엔드 개발자 입니다. 매 순간 도전의 과정에서 고민과
-        합리적인 사고를 통해 성장하고, 새로운 기술을 공부하는 것을 좋아합니다.
+    <section className="py-20">
+      <Lottie
+        loop
+        animationData={backgroundLottie}
+        play
+        style={{ position: 'absolute', top: 0, width: '100vw', zIndex: -1 }}
+      />
+      <div className="flex-col w-[50rem]">
+        <h1 className="flex justify-center text-4xl font-bold text-teal-50 pb-10">
+          <span>안녕하세요, 프론트엔드 개발자 &nbsp;</span>
+          <span className="text-teal-300">이규호</span>
+          <span>입니다.</span>
+        </h1>
+        <div className="flex text-teal-50 text-2xl font-medium justify-start">
+          안녕하세요 소개소개 이러쿵저러쿵
+        </div>
+        <div className="flex text-teal-50 text-2xl font-medium justify-end">
+          랄랄라 움냠냠 동당당
+        </div>
+        <div className="w-full flex justify-center p-16 gap-4">
+          <button className="bg-teal-500 rounded-[1rem] w-[10rem] h-[3rem]">
+            Projects
+          </button>
+          <button className="bg-teal-500 rounded-[1rem] w-[10rem] h-[3rem]">
+            Experience
+          </button>
+        </div>
       </div>
     </section>
   );
--- src/components/home/TechSection.tsx
+++ src/components/home/TechSection.tsx
@@ -1,8 +1,8 @@
 const TechSection: React.FC = () => {
   return (
-    <section>
-      <h1 className="text-teal-50">TECH elcie font</h1>
-      <h1 className="text-teal-50 text-5xl">테스트 elice text</h1>
+    <section id="tech-section">
+      <h1 className="text-teal-50 text-3xl">TECH</h1>
+      <h1 className="text-teal-0 text-xl">테스트 elice text</h1>
     </section>
   );
 };
