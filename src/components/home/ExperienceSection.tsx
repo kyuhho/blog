5334bf24151dfe2777d28256edf163fbb66b549b
kyuhho <rbgh0114@naver.com>
2024-04-16 00:13:24 +0900

    feat: apply `NumberBox` shared component

--- src/components/home/ExperienceSection.tsx
+++ src/components/home/ExperienceSection.tsx
@@ -1,13 +1,14 @@
 import { experienceMockData } from '@/utils/mock';
 import { ExperienceCardItem } from '@/utils/types';
 import ExperienceCard from './experience/ExperienceCard';
+import NumberBox from '../shared/NumberBox';
 
 const ExperienceSection: React.FC = () => {
   const ExperienceItems: ExperienceCardItem[] = experienceMockData;
 
   return (
-    <section id="experience-section" className="w-full">
-      <h1 className="text-black font-bold text-5xl mb-6">Experience</h1>
+    <section id="experience-section" className="w-full mt-[5rem]">
+      <NumberBox number={1} bgColor="green" />
       <ul className="grid grid-cols-template-experiences grid-rows-1 gap-4">
         {ExperienceItems.map((item) => (
           <ExperienceCard key={item.name} item={item} />
---
+++ src/components/shared/NumberBox.tsx
@@ -0,0 +1,43 @@
+interface NumberBoxProps {
+  number: number;
+  bgColor: 'inherit' | 'green';
+}
+
+//
+//
+//
+
+const NumberBox: React.FC<NumberBoxProps> = ({ number, bgColor }) => {
+  /**
+   *
+   */
+  const getBgClassName = () => {
+    switch (bgColor) {
+      case 'green': {
+        return 'bg-primary-color';
+      }
+      case 'inherit': {
+        return 'bg-inherit';
+      }
+      default: {
+        return 'bg-inherit';
+      }
+    }
+  };
+
+  const bgClassName = getBgClassName();
+
+  //
+  //
+  //
+
+  return (
+    <span
+      className={`border-solid border-[1px] border-black ${bgClassName} rounded-[0.75rem] px-[1rem] py-[0.375rem] font-gilroy font-medium text-[2rem]`}
+    >
+      {number}
+    </span>
+  );
+};
+
+export default NumberBox;
