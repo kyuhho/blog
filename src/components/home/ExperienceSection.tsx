3002c988327455eee87b7224c51e48c2c4b062c0
kyuhho <rbgh0114@naver.com>
2024-04-16 00:33:14 +0900

    feat: add `TitleBox`

--- src/components/home/ExperienceSection.tsx
+++ src/components/home/ExperienceSection.tsx
@@ -2,14 +2,18 @@ import { experienceMockData } from '@/utils/mock';
 import { ExperienceCardItem } from '@/utils/types';
 import ExperienceCard from './experience/ExperienceCard';
 import NumberBox from '../shared/NumberBox';
+import TitleBox from '../shared/TitleBox';
 
 const ExperienceSection: React.FC = () => {
   const ExperienceItems: ExperienceCardItem[] = experienceMockData;
 
   return (
     <section id="experience-section" className="w-full mt-[5rem]">
+      {/* <div className=""> */}
       <NumberBox number={1} bgColor="green" />
-      <ul className="grid grid-cols-template-experiences grid-rows-1 gap-4">
+      <TitleBox content="Experience" bgColor="inherit" />
+      {/* </div> */}
+      <ul className="flex flex-col">
         {ExperienceItems.map((item) => (
           <ExperienceCard key={item.name} item={item} />
         ))}
---
+++ src/components/shared/TitleBox.tsx
@@ -0,0 +1,43 @@
+interface TitleBoxProps {
+  content: string;
+  bgColor: 'inherit' | 'green';
+}
+
+//
+//
+//
+
+const TitleBox: React.FC<TitleBoxProps> = ({ content, bgColor }) => {
+  /**
+   *
+   */
+  const getBgClassName = () => {
+    switch (bgColor) {
+      case 'green': {
+        return 'bg-primary-color';
+      }
+      case 'inherit': {
+        return 'bg-inherit';
+      }
+      default: {
+        return 'bg-inherit';
+      }
+    }
+  };
+
+  const bgClassName = getBgClassName();
+
+  //
+  //
+  //
+
+  return (
+    <span
+      className={`border-solid border-[1px] border-black ${bgClassName} rounded-[0.75rem] px-[1.5rem] py-[0.28rem] font-gilroy font-extrabold text-[2.25rem]`}
+    >
+      {content}
+    </span>
+  );
+};
+
+export default TitleBox;
